a35ff497df45d34126f16b04fd7b977ff33dda1b
Aaron <roycet000@gmail.com>
2020-04-01 20:04:27 -0700

    Add files via upload
    Week 2 Homework Boolean Operators, Conditionals, and Loops

---
+++ Week02HW.java
@@ -0,0 +1,20 @@
+package week02HomeworkBooleanConditionalsLoops;
+
+public class Week02HW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String weekday1 = "Monday";
+		String weekday2 = "Tuesday";
+		String weekday3 = "Wednesday";
+		String weekday4 = "Thursday";
+		String weekday5 = "Friday";
+		String weekend1 = "Saturday";
+		String weekend2 = "Sunday";
+		
+		
+		
+	}
+
+}
