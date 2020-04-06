3cde540f6208be489d65b19860cb09b928f2233b
Aaron <roycet000@gmail.com>
2020-04-05 22:12:05 -0700

    Add files via upload

--- Loops.java
+++ Loops.java
@@ -9,7 +9,7 @@ public class Loops {
 		 *a.	A while loop that prints all even numbers from 0 to 100
 		 *b.	A while loop that prints every 3rd number going backwards from 100 until we reach 0
 		 *c.	A for loop that prints every other number from 1 to 100
-		 *d.	A for loop that prints every number from 0 to 100, but if the number is divisible by 3, it prints “Hello” instead of the number, and if the number is divisible by 5, it prints “World” instead of the number, and if it is divisible by both 3 and 5, it prints “HelloWorld” instead of the number.
+		 *d.	A for loop that prints every number from 0 to 100, but if the number is divisible by 3, it prints ï¿½Helloï¿½ instead of the number, and if the number is divisible by 5, it prints ï¿½Worldï¿½ instead of the number, and if it is divisible by both 3 and 5, it prints ï¿½HelloWorldï¿½ instead of the number.
 		 */
 		
 		int evenNumber = 0;
@@ -42,6 +42,23 @@ public class Loops {
 		//	System.out.println(everyOtherNumber);
 			//everyOtherNumber=everyOtherNumber+2;
 		//}	
+	
+		for (int number = 1; number <= 100; number++ ) {
+			if (number % 3 == 0) { 
+				System.out.println("?Hello?");
+				}
+		    if (number % 5 == 0 ) {
+				System.out.println("?World?");
+		    }
+				else {
+					System.out.println(number);
+				}
+			
+		
+		    }	
+	
+	
 	}
+	
 
 }
